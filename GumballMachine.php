6375eedc378e76ba761ab5b65d7a8c385b0b7c67
belarbi2733 <belarbi_mohammed_amin@univ-mosta.dz>
2019-10-01 15:21:37 +0200

    sss

--- GumballMachine.php
+++ GumballMachine.php
@@ -11,7 +11,7 @@ class GumballMachine
 	{
 	    try
 	    {
-	        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+	        $this->bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 	        print "Yes Dans le constructeur de BaseClass\n";
 	    }
 	    
@@ -34,9 +34,9 @@ class GumballMachine
 	}
 	public function InsertP( $nom, $prenom , $date_naissance,$lieu)
 	{  
-	    /*$sql = "INSERT INTO prof (nom, prenom, data_naissance, lieu_naissance) VALUES (?,?,?,?)";
+	    $sql = "INSERT INTO prof (nom, prenom, data_naissance, lieu_naissance) VALUES (?,?,?,?)";
 	    $stmt= $this->$bdd->prepare($sql);
-	    $stmt->execute([$nom, $prenom, $date_naissance, $lieu]);*/
+	    $stmt->execute([$nom, $prenom, $date_naissance, $lieu]);
 	    
 	}
 	
