dc0869615401841f82a00cd0503a9f8690ce72ca
belarbi2733 <belarbi_mohammed_amin@univ-mosta.dz>
2019-10-01 15:14:13 +0200

    sss

--- GumballMachine.php
+++ GumballMachine.php
@@ -1,6 +1,7 @@
 <?php
 //test
-class GumballMachine{
+class GumballMachine
+{
 
 	private $gumballs;
 	
@@ -21,36 +22,18 @@ class GumballMachine{
 	}
 
 
-	// Get the amount of gumballs still in the machine
-	public function getData()
-	{
-	    try
-	    {
-	        $this->$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-	        print "Yes Dans le constructeur de BaseClass\n";
-	    }
-	    
-	    catch (Exception $e)
-	    {
-	        die('Erreur : ' . $e->getMessage());
-	    }
-	    //return $this->$bdd;
-	}
-
-	// Set the amount of gumballs in the machine
 	public function setGumballs($amount)
 	{
 		$this->gumballs = $amount;
 	}
 
-	// The user turns the wheel, machine dispenses gumball!
+	
 	public function turnWheel()
 	{
 		$this->setGumballs($this->getGumballs() - 1);
 	}
 	public function InsertP( $nom, $prenom , $date_naissance,$lieu)
-	{
-	    $this->getData();   
+	{  
 	    $sql = "INSERT INTO prof (nom, prenom, data_naissance, lieu_naissance) VALUES (?,?,?,?)";
 	    $stmt= $this->$bdd->prepare($sql);
 	    $stmt->execute([$nom, $prenom, $date_naissance, $lieu]);
