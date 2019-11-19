31e7c53af89641114c7eae2f67cdaac06da46c90
Belarbi Moammed Amin <belarbi_mohammed_amin@univ-mosta.dz>
2019-11-19 20:54:24 +0100

    all tests
    tests

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -6,6 +6,15 @@ require 'GumballMachine.php';
 class GumballMachineTest extends PHPUnit_Framework_TestCase
 {
     public $gumballMachineInstance;
+    public $gumballMachineInstance;
+    //prof
+    private $nom="x_test_tp"; // a changer
+    private $prenom="y_test_tp"; // a changer
+    private $date_naissance="0000-00-00"; // a changer
+    private $lieu_naissance="XY"; // a changer
+    // cours
+    private $intitule="***"; //a remplir
+    private $duree="***";    //a remplir
 
     public function setUp()
     {
@@ -14,27 +23,42 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     
     public function testAffichageProfAVI()
     {
-        $this->gumballMachineInstance->AffichageProf("Before Insertion of Professors");
+        $this->assertEquals(true,$this->gumballMachineInstance->AffichageProf("Before Insertion of Professors"));
     }
     public function testInsertP()
     {
-        $this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),"yassine","Amakrane","1994-04-22","Zagoura");
-       
+        $max__id1=$this->gumballMachineInstance->GetLastIDP();
+        $this->assertEquals(true,$this->gumballMachineInstance->InsertP($this->gumballMachineInstance->getDB(),$this->nom,$this->prenom,$this->date_naissance,$this->lieu_naissance));
+        $max__id2=$this->gumballMachineInstance->GetLastIDP();
+        $this->assertEquals($max__id1+1,$max__id2);
     }
     public function testAffichageProfAPI()
     {
-        $this->gumballMachineInstance->AffichageProf("After Insertion of Professors");
+        $this->assertEquals(true,$this->gumballMachineInstance->AffichageProf("After Insertion of Professors"));
     }
     
     
     
     public function testAffichageCoursAVI()
     {
-        $this->gumballMachineInstance->AffichageCours("Before Insertion of Courses");
+        $this->assertEquals(true,$this->gumballMachineInstance->AffichageCours("Before Insertion of Courses"));
     }
     public function testInsertC()
     {
+        $max__id1=$this->gumballMachineInstance->GetLastIDC();
         $this->gumballMachineInstance->InsertC("IOT","12",$this->gumballMachineInstance->GetIdP("yassine","Amakrane"));
+        $max__id2=$this->gumballMachineInstance->GetLastIDC();
+        $this->assertEquals($max__id1+1,$max__id2);
+        
+    }
+    public function testUpdateP()
+    {
+        $this->assertEquals(true,$this->gumballMachineInstance->UpdateP("New_Name1","New_Name2",$this->gumballMachineInstance->GetIdP("New_Name1","New_Name2")));
+        
+    }
+    public function testDeleteP()
+    {
+        $this->assertEquals(true,$this->gumballMachineInstance->DeleteP($this->gumballMachineInstance->GetIdP("New_Name1","New_Name2")));
         
     }
     public function testAffichageCoursAPI()
