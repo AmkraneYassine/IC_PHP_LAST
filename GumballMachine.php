c24d8473f4b6c96e66cf7b79b67a9874b9ddb750
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-04 19:37:14 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -2,10 +2,14 @@
 //test
 class GumballMachine
 {
+	
+	
+	
 
 	private $gumballs;
 	
 	private $bdd;
+	
 	
 	/*
 	Paramètre de connexion à la base de données
@@ -17,10 +21,24 @@ class GumballMachine
 	
 	function __construct()
 	{
+		$host = "192.168.250.3";
+		$root = "user01";
+		$root_password = "user01";
+		mysql:host=localhost;dbname=test
+
+		try {
+			  $dbh = new PDO("mysql:host=$host", $root, $root_password);
+			  $dbh->exec("CREATE DATABASE `$db`;")
+			  or die(print_r($dbh->errorInfo(), true));
+		}
+		catch (PDOException $e) {
+   		 die("DB ERROR: " . $e->getMessage());
+		}
+		
 	    try
 	    {
 		// "mysql:host=$this->servername;dbname=$this->db_name", $this->db_user, $this->db_pass
-	        $this->bdd = new PDO('mysql:host=192.168.250.3;dbname=user01_test_java;charset=utf8', 'user01', 'user01');
+	        $this->bdd = new PDO('mysql:host=192.168.250.3;dbname=test;charset=utf8', 'user01', 'user01');
 	        //print "Yes Dans le constructeur de BaseClass\n";
 	    }
 	    
