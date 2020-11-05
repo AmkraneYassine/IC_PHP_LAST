cc1991096d7e7109ebb056871cd4424f48d24cc4
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-05 14:34:23 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -124,8 +124,8 @@ class GumballMachine
 	
 	public function GetIdP($nom,$prenom)
 	{
-	    $stmt = $this->bdd->prepare("select id from prof where nom=? and prenom=?");
-	    $stmt->execute([$nom,$prenom]); 
+	    $stmt = $this->bdd->prepare("select id from prof where nom=? and prenom=? limits 1");
+	    $stmt->execute([$nom,$prenom]);
 	    $user = $stmt->fetch();
 	    return $user['id'];
 	}
