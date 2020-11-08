346b0e813f4c39569aa804c4c39d5f118267025e
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 19:11:36 +0100

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -21,42 +21,13 @@ class GumballMachineTest extends TestCase
     public function setUp(): void
     {
          $this->gumballMachineInstance = new GumballMachine();
+         $this->assertEquals(false,$this->gumballMachineInstance->InsertData());
     }
     
-    public function InsertProfs()
-    {  
-	$max__id1=$this->gumballMachineInstance->GetLastIDP();
-	$this->gumballMachineInstance->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX1','YYY1', '1980-09-29','ZZZA')");
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX2','YYY2', '1981-10-30','ZZZB')");
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX3','YYY3', '1980-09-29','ZZZC')");
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX4','YYY4', '1991-07-13','ZZZD')");
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('AMKRANE','Yassine', '1993-03-20', 'ZZZE')");
-	$max__id2=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertEquals($max__id1+5,$max__id2);
-    }
-	
     public function testAffichageProfAVI()
     {
         $this->assertEquals(true,$this->gumballMachineInstance->AffichageProf("Before Insertion of Professors"));
     }
-
-		
-    public function InsertCours()
-    {
-        $max__id1=$this->gumballMachineInstance->GetLastIDC();
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('IOT','10', $this->gumballMachineInstance->GetLastIDP()))");
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('IA','12', $this->gumballMachineInstance->GetLastIDP()))");
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('Cpp','18', $this->gumballMachineInstance->GetLastIDP()))");
-        $this->gumballMachineInstance->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('EDL','30', $this->gumballMachineInstance->GetLastIDP()))");
-	$max__id2=$this->gumballMachineInstance->GetLastIDP();
-        $this->assertEquals($max__id1+4,$max__id2);
-    }
-    
-    public function testAffichageCoursAVI()
-    {
-        $this->assertEquals(true,$this->gumballMachineInstance->AffichageCours("Before Insertion of Courses"));
-    }
-    
     public function testInsertP()
     {
         $max__id1=$this->gumballMachineInstance->GetLastIDP();
@@ -68,33 +39,36 @@ class GumballMachineTest extends TestCase
     {
         $this->assertEquals(true,$this->gumballMachineInstance->AffichageProf("After Insertion of Professors"));
     }
-
+    
+    public function testAffichageCoursAVI()
+    {
+        $this->assertEquals(true,$this->gumballMachineInstance->AffichageCours("Before Insertion of Courses"));
+    }
     
     public function testInsertC()
     {
         $max__id1=$this->gumballMachineInstance->GetLastIDC();
-        $this->assertEquals(true, $this->gumballMachineInstance->InsertC("PHP","12", $this->gumballMachineInstance->GetIdP("XXX3","YYY3")));
+        $this->assertEquals(true, $this->gumballMachineInstance->InsertC("PHP","12", $this->gumballMachineInstance->GetIdP("PROF_TEST","Prof_Test")));
         $max__id2=$this->gumballMachineInstance->GetLastIDC();
         $this->assertEquals($max__id1+1,$max__id2);
     }
     public function testUpdateP()
     {
-        $this->assertEquals(true,$this->gumballMachineInstance->UpdateP("Nolack","Fabrice",$this->gumballMachineInstance->GetIdP('XXX4','YYY4')));
+        $this->assertEquals(true,$this->gumballMachineInstance->UpdateP("Nolack","Fabrice",$this->gumballMachineInstance->GetIdP("XXX4","YYY4")));
     }
     public function testUpdateC()
     {
-        $this->assertEquals(true,$this->gumballMachineInstance->UpdateC("Cpp","30"));
+        $this->assertEquals(true,$this->gumballMachineInstance->UpdateC("C++","30"));
     }
     public function testDeleteP()
     {
-        $this->assertEquals(true,$this->gumballMachineInstance->DeleteP($this->gumballMachineInstance->GetIdP("AMKRANE","Yassine")));
+        $this->assertEquals(true,$this->gumballMachineInstance->DeleteP($this->gumballMachineInstance->GetIdP("PROF_TEST","Prof_Test")));
     }
-    
-	
     public function testAffichageCoursAPI()
     {
         $this->assertEquals(true, $this->gumballMachineInstance->AffichageCours("After Insertion of Courses"));
     }
+    
     
     public function testDelete_All()
     {
