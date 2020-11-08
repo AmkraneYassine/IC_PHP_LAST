5024b0e073d9b703d5f66fdbd7b4504101b4bb3a
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 11:53:09 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -212,9 +212,6 @@ class GumballMachine
 	        //$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 	        $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		// $sql = "delete from prof where id=$id";
-		$sql = "delete * from cours";
-		$this->bdd->exec($sql);
-	        $sql = "delete * from prof";
 	        $this->bdd->exec($sql);
 		echo "\n We Have a new insertion of Corse";
 		return true;
