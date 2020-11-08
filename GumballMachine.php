6153a666024df01f73c360fd36254bb8ec502656
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 17:54:04 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -47,9 +47,9 @@ class GumballMachine
 		    // "mysql:host=$this->servername;dbname=$this->db_name", $this->db_user, $this->db_pass
 		    // $sql = "DROP SCHEMA IF EXISTS user01_test_php";
 		    // $this->bdd->exec('DROP SCHEMA IF EXISTS user01_test_php');
-		    // $sql="CREATE SCHEMA IF NOT EXISTS `user01_test_php` DEFAULT CHARACTER SET utf8 ; USE `user01_test_php`";
-		    // $this->bdd->exec($sql) or die(print_r($this->bdd->errorInfo(), true));
-		    $this->bdd = new PDO('mysql:host=192.168.250.3;dbname=user01_test_php;charset=utf8', 'user01', 'user01');
+		    $sql="CREATE SCHEMA IF NOT EXISTS `user01_test_php` DEFAULT CHARACTER SET utf8 ; USE `user01_test_php`";
+		    $this->bdd->exec($sql) or die(print_r($this->bdd->errorInfo(), true));
+		    $this->bdd = new PDO("mysql:host=192.168.250.3;dbname=user01_test_php;charset=utf8", 'user01', 'user01');
 		    // print "Yes Dans le constructeur de BaseClass\n";
 		    $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		    $sql="CREATE TABLE  IF NOT EXISTS prof( id INT NOT NULL AUTO_INCREMENT , nom VARCHAR(25) NOT NULL , prenom VARCHAR(25) NOT NULL , date_naissance DATE NOT NULL , lieu_naissance TEXT NOT NULL , PRIMARY KEY (id)) ";
