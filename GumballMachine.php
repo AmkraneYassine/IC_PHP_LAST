ec220102cd2da499a336718d2da9ef70d2243463
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 15:05:00 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -58,52 +58,7 @@ class GumballMachine
 			die('Erreur : ' . $e->getMessage());
 	    }
 	}
-	
-	public function InsertProfs()
-	{  
-	    try 
-	    {
-	       	//$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-		$this->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX1','YYY1', '1980-09-29','ZZZA')");
-        	$this->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX2','YYY2', '1981-10-30','ZZZB')");
-        	$this->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX3','YYY3', '1980-09-29','ZZZC')");
-        	$this->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX4','YYY4', '1991-07-13','ZZZD')");
-        	$this->bdd->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('AMKRANE','Yassine', '1993-03-20', 'ZZZE')");
-
-	       echo "\n 0 - Insertions Profs";
-	       return true;
-	    }
-	    catch(PDOException $e)
-	    {
-	        echo "<br>" . $e->getMessage();
-		return false;
-	    }
-	    
-	}
-	
-	public function InsertCours()
-	{  
-	    try 
-	    {
-	       	//$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-		$this->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('IOT','10', GetLastIDP()))");
-        	$this->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('IA','12', GetLastIDP()))");
-        	$this->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('C++','18', GetLastIDP()))");
-        	$this->bdd->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('EDL','30', GetLastIDP()))");
-
-	       echo "\n 0 - Insertions Cours";
-	       return true;
-	    }
-	    catch(PDOException $e)
-	    {
-	        echo "<br>" . $e->getMessage();
-		return false;
-	    }
-	    
-	}
-	
-
-        	
+	        	
     	public function getDB()
     	{
    	     	return $this->bdd;
