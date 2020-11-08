c01d7738b0024d1f62e2168b5802295bc885ba62
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 11:25:37 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -41,7 +41,7 @@ class GumballMachine
 	    try
 	    {
 			// "mysql:host=$this->servername;dbname=$this->db_name", $this->db_user, $this->db_pass
-	        	$this->bdd = new PDO('mysql:host=192.168.250.3;dbname=user01_test_php;charset=utf8', 'user01', 'user01');
+	        	$this->bdd = new PDO('mysql:host=192.168.250.3;dbname=user01_test_php1;charset=utf8', 'user01', 'user01');
 			 /*print "Yes Dans le constructeur de BaseClass\n";*/
 			$this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$sql="CREATE TABLE  IF NOT EXISTS prof( id INT NOT NULL AUTO_INCREMENT , nom VARCHAR(25) NOT NULL , prenom VARCHAR(25) NOT NULL , date_naissance DATE NOT NULL , lieu_naissance TEXT NOT NULL , PRIMARY KEY (id)) ";
