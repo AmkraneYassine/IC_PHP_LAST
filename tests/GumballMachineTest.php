24f70fe413d0cda1897fa37348e9141c514509eb
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 23:41:17 +0100

    Update GumballMachineTest.php

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -1,5 +1,7 @@
 <?php
 
+/* hello */
+
 use PHPUnit\Framework\TestCase;
 
 require 'GumballMachine.php';
@@ -15,11 +17,22 @@ class GumballMachineTest extends TestCase
     // cours
     private $intitule="***"; //a remplir
     private $duree="***";    //a remplir
-    
-        
-    public function setUp() : void
+
+    public function setUp(): void
     {
-        $this->gumballMachineInstance = new GumballMachine();
+         $this->gumballMachineInstance = new GumballMachine();
+    }
+	
+    public function testInsertProfs()
+    {
+	$this->gumballMachineInstance->getDB()->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX1','YYY1', '1980-09-29','ZZZA')");
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX2','YYY2', '1981-10-30','ZZZB')");
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX3','YYY3', '1980-09-29','ZZZC')");
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('XXX4','YYY4', '1991-07-13','ZZZD')");
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO prof (nom, prenom, date_naissance, lieu_naissance) VALUES ('AMKRANE','Yassine', '1993-03-20', 'ZZZE')");
+
+
+	echo "\n 0 - Insertions Profs";
     }
     
     public function testAffichageProfAVI()
@@ -35,24 +48,53 @@ class GumballMachineTest extends TestCase
     }
     public function testAffichageProfAPI()
     {
-        /*à completer*/
+        $this->assertEquals(true,$this->gumballMachineInstance->AffichageProf("After Insertion of Professors"));
     }
-     
+    
+    
+    public function testInsertCours()
+	{
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('IOT','10', GetIdP('XXX2','YYY2'))");
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('IA','12', GetIdP('XXX1','YYY1'))");
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('C++','18', GetIdP('XXX3','YYY3'))");
+        $this->gumballMachineInstance->getDB()->exec("INSERT INTO cours (intitule, duree, id_prof) VALUES ('EDL','30', GetIdP('XXX3','YYY3'))");
+	    
+        echo "\n 0 - Insertions Cours";
+	}
     
     public function testAffichageCoursAVI()
     {
-        /*à completer*/
+        $this->assertEquals(true,$this->gumballMachineInstance->AffichageCours("Before Insertion of Courses"));
     }
+    
     public function testInsertC()
     {
-       
-        /*à completer*/
-        
+        $max__id1=$this->gumballMachineInstance->GetLastIDC();
+        $this->assertEquals(true, $this->gumballMachineInstance->InsertC("PHP","12", $this->gumballMachineInstance->GetIdP("XXX3","YYY3")));
+        $max__id2=$this->gumballMachineInstance->GetLastIDC();
+        $this->assertEquals($max__id1+1,$max__id2);
+    }
+    public function testUpdateP()
+    {
+        $this->assertEquals(true,$this->gumballMachineInstance->UpdateP("Nolack","Fabrice",$this->gumballMachineInstance->GetIdP('XXX4','YYY4')));
+    }
+    public function testUpdateC()
+    {
+        $this->assertEquals(true,$this->gumballMachineInstance->UpdateC("C++","30"));
+    }
+    public function testDeleteP()
+    {
+        $this->assertEquals(true,$this->gumballMachineInstance->DeleteP($this->gumballMachineInstance->GetIdP("AMKRANE","Yassine")));
     }
     public function testAffichageCoursAPI()
     {
-        /*à completer*/
+        $this->assertEquals(true, $this->gumballMachineInstance->AffichageCours("After Insertion of Courses"));
     }
-
-   
+    
+    
+    public function testDelete_All()
+    {
+        $this->assertEquals(true, $this->gumballMachineInstance->delete_all());
+        echo "Tous les enregistrements sont supprimés";
+    }
 }
