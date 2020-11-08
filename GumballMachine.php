c83845166a76447b73f975de3191873fcdb156b4
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 11:16:49 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -57,7 +57,7 @@ class GumballMachine
 	    }
 	    
 	    
-	function __destruct (): void
+	function __destruct ()
     	{
         	$this->$bdd->exec('DROP DATABASE IF EXISTS '.self::DBNAME);
     	}
