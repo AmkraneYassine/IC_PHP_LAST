f0b345eea6e3bfd422bd7f4359f56066a44ca92c
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 18:17:49 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -16,12 +16,14 @@ class GumballMachine
 	
 	function __construct()
 	{
-		/*
+		
 		$dbname = "user01_test_php" ;
 		$host = "192.168.250.3";
 		$root = "user01";
 		$root_password = "user01";
 		
+		
+		/*
 		try {
 			// $this->bdd = new PDO("mysql:host=$host; dbname=$dbname", $root, $root_password);
 			$this->bdd->exec("CREATE SCHEMA IF NOT EXISTS `user01_test_php` DEFAULT CHARACTER SET utf8 ; USE `user01_test_php` ;") 
