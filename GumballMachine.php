94e47a3b3285d596802fec02e4c19d10992c449c
AmkraneYassine <48215554+AmkraneYassine@users.noreply.github.com>
2020-11-08 12:04:36 +0100

    Update GumballMachine.php

--- GumballMachine.php
+++ GumballMachine.php
@@ -39,8 +39,8 @@ class GumballMachine
 	    {
 			// "mysql:host=$this->servername;dbname=$this->db_name", $this->db_user, $this->db_pass
 		    	// $this->bdd->exec("CREATE SCHEMA IF NOT EXISTS `user01_test_php` DEFAULT CHARACTER SET utf8 ; USE `user01_test_php` ;")  or die(print_r($this->bdd->errorInfo(), true));
-		    	// $sql = "DROP DATABASE IF EXISTS user01_test_php";
-		    	// $this->bdd->exec($sql);
+		    	$sql = "DROP SCHEMA IF EXISTS user01_test_php";
+		    	$this->bdd->exec($sql);
 	        	$this->bdd = new PDO('mysql:host=192.168.250.3;dbname=user01_test_php;charset=utf8', 'user01', 'user01');
 			 /*print "Yes Dans le constructeur de BaseClass\n";*/
 			$this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
